3fc46a99863436f741053f88cc3409a15763879e
braxtons12 <6ypqal4t>
2020-07-15 00:45:47 -0500

    Add/Impl GainComputerCompressor

--- src/dsp/dynamics/gaincomputers/GainComputerCompressor.cpp
+++ src/dsp/dynamics/gaincomputers/GainComputerCompressor.cpp
@@ -1,1 +1,68 @@
- 
+#include "GainComputerCompressor.h"
+
+namespace apex {
+	namespace dsp {
+
+		///@brief Constructs a `GainComputerCompressor` with the following defaults:
+		/// * ratio: 1.1
+		/// * threshold: -12dB
+		/// * knee width: 6dB
+		GainComputerCompressor<float>::GainComputerCompressor(float ratio, float threshold, 
+			float kneeWidth) noexcept
+		{
+			mRatio = ratio;
+			mThreshold = threshold;
+			mKneeWidth = kneeWidth;
+		}
+
+		///@brief Calculates the target compressed output value 
+		///
+		///@param input - The input to calculate compression for
+		///
+		///@return - The target output
+		float GainComputerCompressor<float>::process(float input) noexcept {
+			float twoXMinusT = 2.0f * (input - mThreshold);
+			if(twoXMinusT < -mKneeWidth) {
+				return input;
+			}
+			else if(twoXMinusT > mKneeWidth) {
+				return mThreshold + (input - mThreshold) / mRatio;
+			}
+			else {
+				return input + ( (1.0f / mRatio) - 1.0f)
+					* math::pow2f(input - mThreshold + mKneeWidth / 2.0f) / (2.0f * mKneeWidth);
+			}
+		}
+
+		///@brief Constructs a `GainComputerCompressor` with the following defaults:
+		/// * ratio: 1.1
+		/// * threshold: -12dB
+		/// * knee width: 6dB
+		GainComputerCompressor<double>::GainComputerCompressor(double ratio, double threshold, 
+			double kneeWidth) noexcept
+		{
+			mRatio = ratio;
+			mThreshold = threshold;
+			mKneeWidth = kneeWidth;
+		}
+
+		///@brief Calculates the target compressed output value 
+		///
+		///@param input - The input to calculate compression for
+		///
+		///@return - The target output
+		double GainComputerCompressor<double>::process(double input) noexcept {
+			double twoXMinusT = 2.0 * (input - mThreshold);
+			if(twoXMinusT < -mKneeWidth) {
+				return input;
+			}
+			else if(twoXMinusT > mKneeWidth) {
+				return mThreshold + (input - mThreshold) / mRatio;
+			}
+			else {
+				return input + ( (1.0 / mRatio) - 1.0)
+					* math::pow2(input - mThreshold + mKneeWidth / 2.0) / (2.0 * mKneeWidth);
+			}
+		}
+	}
+}
--- src/dsp/dynamics/gaincomputers/GainComputerCompressor.h
+++ src/dsp/dynamics/gaincomputers/GainComputerCompressor.h
@@ -1,1 +1,90 @@
- 
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+#include "GainComputer.h"
+#include "../../../base/StandardIncludes.h"
+
+namespace apex {
+	namespace dsp {
+		///@brief Gain Computer for compressors
+		///
+		///@tparam T - The floating point type to back operations
+		template<typename T>
+			class GainComputerCompressor : GainComputer<T> {
+				public:
+					static_assert(std::is_floating_point<T>::value, "T must be a floating point type");
+
+				private:
+					JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerCompressor)
+			};
+
+		///@brief Gain Computer for compressors
+		template<>
+			class GainComputerCompressor<float> : GainComputer<float> {
+				public:
+					///@brief Constructs a `GainComputerCompressor` with the following defaults:
+					/// * ratio: 1.1
+					/// * threshold: -12dB
+					/// * knee width: 6dB
+					GainComputerCompressor() noexcept = default;
+
+					///@brief Construct a `GainComputerCompressor` with the given parameters
+					///
+					///@param ratio - The ratio
+					///@param threshold - The threshold, in Decibels
+					///@param kneeWidth - The knee width, in Decibels
+					GainComputerCompressor(float ratio, float threshold, float kneeWidth) noexcept;
+
+					///@brief Move constructs a `GainComputerCompressor` from the given one
+					///
+					///@param computer - The `GainComputerCompressor` to move
+					GainComputerCompressor(GainComputerCompressor<float>&& computer) noexcept = default;
+					~GainComputerCompressor() noexcept override = default;
+
+					///@brief Calculates the target compressed output value 
+					///
+					///@param input - The input to calculate compression for
+					///
+					///@return - The target output
+					float process(float input) noexcept override;
+
+					GainComputerCompressor<float>& operator=(GainComputerCompressor<float>&& comptuer) noexcept = default;
+			};
+
+		///@brief Gain Computer for compressors
+		template<>
+			class GainComputerCompressor<double> : GainComputer<double> {
+				public:
+					///@brief Constructs a `GainComputerCompressor` with the following defaults:
+					/// * ratio: 1.1
+					/// * threshold: -12dB
+					/// * knee width: 6dB
+					GainComputerCompressor() noexcept = default;
+
+					///@brief Construct a `GainComputerCompressor` with the given parameters
+					///
+					///@param ratio - The ratio
+					///@param threshold - The threshold, in Decibels
+					///@param kneeWidth - The knee width, in Decibels
+					GainComputerCompressor(double ratio, double threshold, double kneeWidth) noexcept;
+
+					///@brief Move constructs a `GainComputerCompressor` from the given one
+					///
+					///@param computer - The `GainComputerCompressor` to move
+					GainComputerCompressor(GainComputerCompressor<double>&& computer) noexcept = default;
+					~GainComputerCompressor() noexcept override = default;
+
+					///@brief Calculates the target compressed output value 
+					///
+					///@param input - The input to calculate compression for
+					///
+					///@return - The target output
+					double process(double input) noexcept override;
+
+					GainComputerCompressor<double>& operator=(GainComputerCompressor<double>&& comptuer) noexcept = default;
+			};
+	}
+
+}
