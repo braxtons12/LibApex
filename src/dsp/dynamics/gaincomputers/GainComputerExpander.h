93054ab745e32cf53547ddbe120626aa8acd07c6
braxtons12 <braxtonsalyer@gmail.com>
2020-07-22 23:43:09 -0500

    Ditto last commit for gain computers
    Signed-off-by: braxtons12 <braxtonsalyer@gmail.com>

--- src/dsp/dynamics/gaincomputers/GainComputerExpander.h
+++ src/dsp/dynamics/gaincomputers/GainComputerExpander.h
@@ -10,206 +10,204 @@
 #ifndef GAIN_COMPUTER_EXPANDER
 #define GAIN_COMPUTER_EXPANDER
 
-namespace apex {
-	namespace dsp {
-		/// @brief Gain Computer for expanders
-		///
-		/// @tparam T - The floating point type to back operations
-		/// @tparam AttackKind - The attack type used by the shared `DynamicsState`
-		/// @tparam ReleaseKind - The release type used by the shared `DynamicsState`
-		template<typename T, typename AttackKind, typename ReleaseKind>
-			class GainComputerExpander : public GainComputer<T, AttackKind, ReleaseKind> {
-				protected:
-					using Field = typename apex::dsp::DynamicsState<T, AttackKind, ReleaseKind>::Field;
-					using DynamicsState = typename apex::dsp::DynamicsState<T, AttackKind, ReleaseKind>;
+namespace apex::dsp {
+	/// @brief Gain Computer for expanders
+	///
+	/// @tparam T - The floating point type to back operations
+	/// @tparam AttackKind - The attack type used by the shared `DynamicsState`
+	/// @tparam ReleaseKind - The release type used by the shared `DynamicsState`
+	template<typename T, typename AttackKind, typename ReleaseKind>
+		class GainComputerExpander : public GainComputer<T, AttackKind, ReleaseKind> {
+			protected:
+				using Field = typename apex::dsp::DynamicsState<T, AttackKind, ReleaseKind>::Field;
+				using DynamicsState = typename apex::dsp::DynamicsState<T, AttackKind, ReleaseKind>;
 
-				public:
-					static_assert(std::is_floating_point<T>::value, "T must be a floating point type");
-					static_assert((std::is_floating_point<AttackKind>::value &&
-								std::is_same<T, AttackKind>::value) ||
-							std::is_enum<AttackKind>::value,
-							"AttackKind must be the same floating point type as T, or an enum");
-					static_assert((std::is_floating_point<ReleaseKind>::value &&
-								std::is_same<T, ReleaseKind>::value) ||
-							std::is_enum<ReleaseKind>::value,
-							"ReleaseKind must be the same floating point type as T, or an enum");
+			public:
+				static_assert(std::is_floating_point<T>::value, "T must be a floating point type");
+				static_assert((std::is_floating_point<AttackKind>::value &&
+							std::is_same<T, AttackKind>::value) ||
+						std::is_enum<AttackKind>::value,
+						"AttackKind must be the same floating point type as T, or an enum");
+				static_assert((std::is_floating_point<ReleaseKind>::value &&
+							std::is_same<T, ReleaseKind>::value) ||
+						std::is_enum<ReleaseKind>::value,
+						"ReleaseKind must be the same floating point type as T, or an enum");
 
-					/// @brief Constructs a `GainComputerExpander` with zeroed shared state
-					GainComputerExpander() noexcept
-						: GainComputer<T, AttackKind, ReleaseKind>()
-						{
+				/// @brief Constructs a `GainComputerExpander` with zeroed shared state
+				GainComputerExpander() noexcept
+					: GainComputer<T, AttackKind, ReleaseKind>()
+					{
 
-						}
-
-					/// @brief Constructs a `GainComputerExpander` with the given shared state
-					///
-					/// @param state - The shared state
-					GainComputerExpander(DynamicsState* state) noexcept
-						: GainComputer<T, AttackKind, ReleaseKind>(state)
-						{
-
-						}
-
-					/// @brief Move constructs the given `GainComputerExpander`
-					///
-					/// @param computer - The `GainComputerExpander` to move
-					GainComputerExpander(GainComputerExpander<T, AttackKind, ReleaseKind>&& computer) noexcept = default;
-					virtual ~GainComputerExpander() override = default;
-
-					/// @brief Calculates the target gain reduction value
-					///
-					/// @param input - The input to calculate gain reduction for
-					///
-					/// @return - The target gain reduction
-					T process(T input) noexcept override;
-
-					GainComputerExpander<T, AttackKind, ReleaseKind>& operator=(
-							GainComputerExpander<T, AttackKind, ReleaseKind>&& computer) noexcept = default;
-
-				private:
-					JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
-			};
-
-		template<typename AttackKind, typename ReleaseKind>
-			class GainComputerExpander<float, AttackKind, ReleaseKind> :
-			public GainComputer<float, AttackKind, ReleaseKind>
-			{
-				protected:
-					typedef typename apex::dsp::DynamicsState<float, AttackKind, ReleaseKind>::Field Field;
-					typedef typename apex::dsp::DynamicsState<float, AttackKind, ReleaseKind> DynamicsState;
-
-				public:
-					static_assert((std::is_floating_point<AttackKind>::value &&
-								std::is_same<float, AttackKind>::value) ||
-							std::is_enum<AttackKind>::value,
-							"AttackKind must be the same floating point type as T, or an enum");
-					static_assert((std::is_floating_point<ReleaseKind>::value &&
-								std::is_same<float, ReleaseKind>::value) ||
-							std::is_enum<ReleaseKind>::value,
-							"ReleaseKind must be the same floating point type as T, or an enum");
-
-					/// @brief Constructs a `GainComputerExpander` with zeroed shared state
-					GainComputerExpander() noexcept
-						: GainComputer<float, AttackKind, ReleaseKind>()
-						{
-
-						}
-
-					/// @brief Constructs a `GainComputerExpander` with the given shared state
-					///
-					/// @param state - The shared state
-					GainComputerExpander(DynamicsState* state) noexcept
-						: GainComputer<float, AttackKind, ReleaseKind>(state)
-						{
-
-						}
-
-					/// @brief Move constructs the given `GainComputerExpander`
-					///
-					/// @param computer - The `GainComputerExpander` to move
-					GainComputerExpander(GainComputerExpander<float, AttackKind, ReleaseKind>&& computer) noexcept = default;
-					virtual ~GainComputerExpander() override = default;
-
-					/// @brief Calculates the target gain reduction value
-					///
-					/// @param input - The input to calculate gain reduction for
-					///
-					/// @return - The target gain reduction
-					float process(float input) noexcept override {
-						float threshold = this->mState->getThreshold();
-						float ratio = this->mState->getRatio();
-						float kneeWidth = this->mState->getKneeWidth();
-
-						float twoXMinusT = 2.0f * (input - this->mState->getThreshold());
-						if(twoXMinusT < -kneeWidth) {
-							return threshold + (input - threshold) * ratio;
-						}
-						else if(twoXMinusT > kneeWidth) {
-							return input;
-						}
-						else {
-							return input + (1.0f - ratio)
-								* math::pow2f(input - threshold - kneeWidth / 2.0f) / (2.0f * kneeWidth);
-						}
 					}
 
-					GainComputerExpander<float, AttackKind, ReleaseKind>& operator=(
-							GainComputerExpander<float, AttackKind, ReleaseKind>&& computer) noexcept = default;
+				/// @brief Constructs a `GainComputerExpander` with the given shared state
+				///
+				/// @param state - The shared state
+				explicit GainComputerExpander(DynamicsState* state) noexcept
+					: GainComputer<T, AttackKind, ReleaseKind>(state)
+					{
 
-				private:
-					JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
-			};
-
-		template<typename AttackKind, typename ReleaseKind>
-			class GainComputerExpander<double, AttackKind, ReleaseKind> :
-			public GainComputer<double, AttackKind, ReleaseKind>
-			{
-				protected:
-					typedef typename apex::dsp::DynamicsState<double, AttackKind, ReleaseKind>::Field Field;
-					typedef typename apex::dsp::DynamicsState<double, AttackKind, ReleaseKind> DynamicsState;
-
-				public:
-					static_assert((std::is_floating_point<AttackKind>::value &&
-								std::is_same<double, AttackKind>::value) ||
-							std::is_enum<AttackKind>::value,
-							"AttackKind must be the same floating point type as T, or an enum");
-					static_assert((std::is_floating_point<ReleaseKind>::value &&
-								std::is_same<double, ReleaseKind>::value) ||
-							std::is_enum<ReleaseKind>::value,
-							"ReleaseKind must be the same floating point type as T, or an enum");
-
-					/// @brief Constructs a `GainComputerExpander` with zeroed shared state
-					GainComputerExpander() noexcept
-						: GainComputer<double, AttackKind, ReleaseKind>()
-						{
-
-						}
-
-					/// @brief Constructs a `GainComputerExpander` with the given shared state
-					///
-					/// @param state - The shared state
-					GainComputerExpander(DynamicsState* state) noexcept
-						: GainComputer<double, AttackKind, ReleaseKind>(state)
-						{
-
-						}
-
-					/// @brief Move constructs the given `GainComputerExpander`
-					///
-					/// @param computer - The `GainComputerExpander` to move
-					GainComputerExpander(GainComputerExpander<double, AttackKind, ReleaseKind>&& computer) noexcept = default;
-					virtual ~GainComputerExpander() override = default;
-
-					/// @brief Calculates the target gain reduction value
-					///
-					/// @param input - The input to calculate gain reduction for
-					///
-					/// @return - The target gain reduction
-					double process(double input) noexcept override {
-						double threshold = this->mState->getThreshold();
-						double ratio = this->mState->getRatio();
-						double kneeWidth = this->mState->getKneeWidth();
-
-						double twoXMinusT = 2.0 * (input - this->mState->getThreshold());
-						if(twoXMinusT < -kneeWidth) {
-							return threshold + (input - threshold) * ratio;
-						}
-						else if(twoXMinusT > kneeWidth) {
-							return input;
-						}
-						else {
-							return input + (1.0 - ratio)
-								* math::pow2(input - threshold - kneeWidth / 2.0) / (2.0 * kneeWidth);
-						}
 					}
 
-					GainComputerExpander<double, AttackKind, ReleaseKind>& operator=(
-							GainComputerExpander<double, AttackKind, ReleaseKind>&& computer) noexcept = default;
+				/// @brief Move constructs the given `GainComputerExpander`
+				///
+				/// @param computer - The `GainComputerExpander` to move
+				GainComputerExpander(GainComputerExpander<T, AttackKind, ReleaseKind>&& computer) noexcept = default;
+				~GainComputerExpander() override = default;
 
-				private:
-					JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
-			};
-	}
-}
+				/// @brief Calculates the target gain reduction value
+				///
+				/// @param input - The input to calculate gain reduction for
+				///
+				/// @return - The target gain reduction
+				auto process(T input) noexcept -> T override;
+
+				auto operator=(GainComputerExpander<T, AttackKind, ReleaseKind>&& computer)
+					noexcept -> GainComputerExpander<T, AttackKind, ReleaseKind>& = default;
+
+			private:
+				JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
+		};
+
+	template<typename AttackKind, typename ReleaseKind>
+		class GainComputerExpander<float, AttackKind, ReleaseKind> :
+		public GainComputer<float, AttackKind, ReleaseKind>
+		{
+			protected:
+				using Field = typename apex::dsp::DynamicsState<float, AttackKind, ReleaseKind>::Field;
+				using DynamicsState = typename apex::dsp::DynamicsState<float, AttackKind, ReleaseKind>;
+
+			public:
+				static_assert((std::is_floating_point<AttackKind>::value &&
+							std::is_same<float, AttackKind>::value) ||
+						std::is_enum<AttackKind>::value,
+						"AttackKind must be the same floating point type as T, or an enum");
+				static_assert((std::is_floating_point<ReleaseKind>::value &&
+							std::is_same<float, ReleaseKind>::value) ||
+						std::is_enum<ReleaseKind>::value,
+						"ReleaseKind must be the same floating point type as T, or an enum");
+
+				/// @brief Constructs a `GainComputerExpander` with zeroed shared state
+				GainComputerExpander() noexcept
+					: GainComputer<float, AttackKind, ReleaseKind>()
+					{
+
+					}
+
+				/// @brief Constructs a `GainComputerExpander` with the given shared state
+				///
+				/// @param state - The shared state
+				explicit GainComputerExpander(DynamicsState* state) noexcept
+					: GainComputer<float, AttackKind, ReleaseKind>(state)
+					{
+
+					}
+
+				/// @brief Move constructs the given `GainComputerExpander`
+				///
+				/// @param computer - The `GainComputerExpander` to move
+				GainComputerExpander(GainComputerExpander<float, AttackKind, ReleaseKind>&& computer) noexcept = default;
+				~GainComputerExpander() override = default;
+
+				/// @brief Calculates the target gain reduction value
+				///
+				/// @param input - The input to calculate gain reduction for
+				///
+				/// @return - The target gain reduction
+				auto process(float input) noexcept -> float override {
+					float threshold = this->mState->getThreshold();
+					float ratio = this->mState->getRatio();
+					float kneeWidth = this->mState->getKneeWidth();
+
+					float twoXMinusT = 2.0F * (input - this->mState->getThreshold());
+					if(twoXMinusT < -kneeWidth) {
+						return threshold + (input - threshold) * ratio;
+					}
+					else if(twoXMinusT > kneeWidth) {
+						return input;
+					}
+					else {
+						return input + (1.0F - ratio)
+							* math::pow2f(input - threshold - kneeWidth / 2.0F) / (2.0F * kneeWidth);
+					}
+				}
+
+				auto operator=(GainComputerExpander<float, AttackKind, ReleaseKind>&& computer)
+					noexcept -> GainComputerExpander<float, AttackKind, ReleaseKind>& = default;
+
+			private:
+				JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
+		};
+
+	template<typename AttackKind, typename ReleaseKind>
+		class GainComputerExpander<double, AttackKind, ReleaseKind> :
+		public GainComputer<double, AttackKind, ReleaseKind>
+		{
+			protected:
+				using Field = typename apex::dsp::DynamicsState<double, AttackKind, ReleaseKind>::Field;
+				using DynamicsState = typename apex::dsp::DynamicsState<double, AttackKind, ReleaseKind>;
+
+			public:
+				static_assert((std::is_floating_point<AttackKind>::value &&
+							std::is_same<double, AttackKind>::value) ||
+						std::is_enum<AttackKind>::value,
+						"AttackKind must be the same floating point type as T, or an enum");
+				static_assert((std::is_floating_point<ReleaseKind>::value &&
+							std::is_same<double, ReleaseKind>::value) ||
+						std::is_enum<ReleaseKind>::value,
+						"ReleaseKind must be the same floating point type as T, or an enum");
+
+				/// @brief Constructs a `GainComputerExpander` with zeroed shared state
+				GainComputerExpander() noexcept
+					: GainComputer<double, AttackKind, ReleaseKind>()
+					{
+
+					}
+
+				/// @brief Constructs a `GainComputerExpander` with the given shared state
+				///
+				/// @param state - The shared state
+				explicit GainComputerExpander(DynamicsState* state) noexcept
+					: GainComputer<double, AttackKind, ReleaseKind>(state)
+					{
+
+					}
+
+				/// @brief Move constructs the given `GainComputerExpander`
+				///
+				/// @param computer - The `GainComputerExpander` to move
+				GainComputerExpander(GainComputerExpander<double, AttackKind, ReleaseKind>&& computer) noexcept = default;
+				~GainComputerExpander() override = default;
+
+				/// @brief Calculates the target gain reduction value
+				///
+				/// @param input - The input to calculate gain reduction for
+				///
+				/// @return - The target gain reduction
+				auto process(double input) noexcept -> double override {
+					double threshold = this->mState->getThreshold();
+					double ratio = this->mState->getRatio();
+					double kneeWidth = this->mState->getKneeWidth();
+
+					double twoXMinusT = 2.0 * (input - this->mState->getThreshold());
+					if(twoXMinusT < -kneeWidth) {
+						return threshold + (input - threshold) * ratio;
+					}
+					else if(twoXMinusT > kneeWidth) {
+						return input;
+					}
+					else {
+						return input + (1.0 - ratio)
+							* math::pow2(input - threshold - kneeWidth / 2.0) / (2.0 * kneeWidth);
+					}
+				}
+
+				auto operator=(GainComputerExpander<double, AttackKind, ReleaseKind>&& computer)
+					noexcept -> GainComputerExpander<double, AttackKind, ReleaseKind>& = default;
+
+			private:
+				JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(GainComputerExpander)
+		};
+} //namespace apex::dsp
 
 #endif //GAIN_COMPUTER_EXPANDER
