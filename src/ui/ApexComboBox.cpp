199a441140ec9eafd55df0652d839ad7183b0b08
braxtons12 <braxtonsalyer@gmail.com>
2020-08-09 23:20:29 -0500

    Modernize/fix clang-tidy lints in `ApexComboBox`

--- src/ui/ApexComboBox.cpp
+++ src/ui/ApexComboBox.cpp
@@ -1,112 +1,178 @@
 #include "ApexComboBox.h"
 
-namespace apex {
-	namespace ui {
+namespace apex::ui {
+	/// @brief Constructs an `ApexComboBox` with the given drop down arrow images
+	///
+	/// @param activeArrowImage - The drop down arrow image to use when the drop down is active
+	/// @param hoveredArrowImage - The drop down arrow image to use when the combo box is
+	/// hovered
+	/// @param normalArrowImage - The drop down arrow image to use normally (ie, not active or
+	/// hovered)
+	ApexComboBox::ApexComboBox(juce::Image activeArrowImage,
+							   juce::Image hoveredArrowImage,
+							   juce::Image normalArrowImage) noexcept
+		: juce::ComboBox(), mActiveArrowImage(std::move(activeArrowImage)),
+		  mHoveredArrowImage(std::move(hoveredArrowImage)),
+		  mNormalArrowImage(std::move(normalArrowImage)), mUsesArrowImages(true) {
+		setJustificationType(juce::Justification::centred);
+	}
 
-		ApexComboBox::ApexComboBox(juce::Image activeArrowImage,
-								   juce::Image hoveredArrowImage,
-								   juce::Image normalArrowImage)
-			: juce::ComboBox(), mActiveArrowImage(activeArrowImage),
-			  mHoveredArrowImage(hoveredArrowImage), mNormalArrowImage(normalArrowImage),
-			  mUsesArrowImages(true) {
-			setJustificationType(juce::Justification::centred);
+	/// @brief Constructs a default `ApexComboBox`
+	ApexComboBox::ApexComboBox() noexcept : juce::ComboBox() {
+		setJustificationType(juce::Justification::centred);
+	}
+
+	/// @brief Mouse button pressed event handler
+	///
+	/// @param e - The mouse event to handle
+	inline auto ApexComboBox::mouseDown(const juce::MouseEvent& e) noexcept -> void {
+		mIsButtonDown = isEnabled() && !e.mods.isPopupMenu();
+
+		juce::ComboBox::mouseDown(e);
+	}
+
+	/// @brief Mouse button released event handler
+	///
+	/// @param e - The mouse event to handle
+	inline auto ApexComboBox::mouseUp(const juce::MouseEvent& e) noexcept -> void {
+		if(mIsButtonDown) {
+			mIsButtonDown = false;
 		}
+		juce::ComboBox::mouseUp(e);
+	}
 
-		ApexComboBox::ApexComboBox() : juce::ComboBox(), mUsesArrowImages(false) {
-			setJustificationType(juce::Justification::centred);
-		}
+	/// @brief Mouse entered component bounds handler
+	///
+	/// @param e - The mouse event to handle
+	inline auto ApexComboBox::mouseEnter(const juce::MouseEvent& e) noexcept -> void {
+		mHovered = true;
+		juce::ComboBox::mouseEnter(e);
+	}
 
-		void ApexComboBox::mouseDown(const juce::MouseEvent& e) {
-			mIsButtonDown = isEnabled() && !e.mods.isPopupMenu();
+	/// @brief Mouse left component bounds handler
+	///
+	/// @param e - The mouse event to handle
+	inline auto ApexComboBox::mouseExit(const juce::MouseEvent& e) noexcept -> void {
+		mHovered = false;
+		juce::ComboBox::mouseExit(e);
+	}
 
-			juce::ComboBox::mouseDown(e);
-		}
+	/// @brief Returns whether the mouse is currently over this component
+	///
+	/// @return - Whether the mouse is over this
+	inline auto ApexComboBox::isHovered() const noexcept -> bool {
+		return mHovered;
+	}
 
-		void ApexComboBox::mouseUp(const juce::MouseEvent& e) {
-			if(mIsButtonDown)
-				mIsButtonDown = false;
-			juce::ComboBox::mouseUp(e);
-		}
+	/// @brief Force-sets whether the mouse is over this component
+	///
+	/// @param hovered - Whether the mouse is over this
+	inline auto ApexComboBox::setHovered(bool hovered) noexcept -> void {
+		mHovered = hovered;
+	}
 
-		void ApexComboBox::mouseEnter(const juce::MouseEvent& e) {
-			mHovered = true;
-			juce::ComboBox::mouseEnter(e);
-		}
+	/// @brief Sets the x-scale factor from the full size, useful when using graphics assets
+	/// instead of drawn elements to determine the ratio they should be scaled by
+	///
+	/// @param scaleFactor - The new scale factor
+	inline auto ApexComboBox::setXScaleFactor(float scaleFactor) noexcept -> void {
+		mXScaleFactor = scaleFactor;
+	}
 
-		void ApexComboBox::mouseExit(const juce::MouseEvent& e) {
-			mHovered = false;
-			juce::ComboBox::mouseExit(e);
-		}
+	/// @brief Returns the current x-scale factor from the full size, useful when using graphics
+	/// assets instead of drawn elements to determine the ratio they should be scaled by
+	///
+	/// @return - The current x-scale factor
+	inline auto ApexComboBox::getXScaleFactor() const noexcept -> float {
+		return mXScaleFactor;
+	}
 
-		bool ApexComboBox::isHovered() {
-			return mHovered;
-		}
+	/// @brief Sets the y-scale factor from the full size, useful when using graphics assets
+	/// instead of drawn elements to determine the ratio they should be scaled by
+	///
+	/// @param scaleFactor - The new scale factor
+	inline auto ApexComboBox::setYScaleFactor(float scaleFactor) noexcept -> void {
+		mYScaleFactor = scaleFactor;
+	}
 
-		void ApexComboBox::setHovered(bool hovered) {
-			mHovered = hovered;
-		}
+	/// @brief Returns the current y-scale factor from the full size, useful when using graphics
+	/// assets instead of drawn elements to determine the ratio they should be scaled by
+	///
+	/// @return - The current y-scale factor
+	inline auto ApexComboBox::getYScaleFactor() const noexcept -> float {
+		return mYScaleFactor;
+	}
 
-		void ApexComboBox::setXScaleFactor(float scaleFactor) {
-			mXScaleFactor = scaleFactor;
-		}
+	/// @brief Sets the font size for text elements controlled/drawn by this component
+	///
+	/// @param newFont - The new font size
+	inline auto ApexComboBox::setFont(juce::Font newFont) noexcept -> void {
+		mFont = std::move(newFont);
+	}
 
-		float ApexComboBox::getXScaleFactor() {
-			return mXScaleFactor;
-		}
+	/// @brief Returns the current font size
+	///
+	/// @return - The current font size
+	inline auto ApexComboBox::getFont() const noexcept -> juce::Font {
+		return mFont;
+	}
 
-		void ApexComboBox::setYScaleFactor(float scaleFactor) {
-			mYScaleFactor = scaleFactor;
-		}
+	/// @brief Sets the look and feel of this component to the given one
+	///
+	/// @param lookNFeel - The new look and feel
+	inline auto
+	ApexComboBox::setLookAndFeel(std::shared_ptr<ApexLookAndFeel> lookNFeel) noexcept -> void {
+		mLookAndFeel = std::move(lookNFeel);
+		juce::Component::setLookAndFeel(mLookAndFeel.get());
+	}
 
-		float ApexComboBox::getYScaleFactor() {
-			return mYScaleFactor;
-		}
+	/// @brief Returns the drop down arrow image to use when the drop down is active, if this is
+	/// using graphics assets instead of drawn components
+	///
+	/// @return - The drop down arrow active image, or `None`
+	inline auto ApexComboBox::getActiveArrowImage() const noexcept -> Option<juce::Image> {
+		return mUsesArrowImages ? Option<juce::Image>::Some(mActiveArrowImage) :
+									Option<juce::Image>::None();
+	}
 
-		void ApexComboBox::setFont(juce::Font newFont) {
-			mFont = newFont;
-		}
+	/// @brief Returns the drop down arrow image to use when this is hovered, if this is
+	/// using graphics assets instead of drawn components
+	///
+	/// @return - The drop down arrow hovered image, or `None`
+	inline auto ApexComboBox::getHoveredArrowImage() const noexcept -> Option<juce::Image> {
+		return mUsesArrowImages ? Option<juce::Image>::Some(mHoveredArrowImage) :
+									Option<juce::Image>::None();
+	}
 
-		juce::Font ApexComboBox::getFont() {
-			return mFont;
-		}
+	/// @brief Returns the drop down arrow image to use normally, if this is
+	/// using graphics assets instead of drawn components
+	///
+	/// @return - The drop down arrow normal image, or `None`
+	inline auto ApexComboBox::getNormalArrowImage() const noexcept -> Option<juce::Image> {
+		return mUsesArrowImages ? Option<juce::Image>::Some(mNormalArrowImage) :
+									Option<juce::Image>::None();
+	}
 
-		void ApexComboBox::setLookAndFeel(std::shared_ptr<ApexLookAndFeel> lookNFeel) {
-			mLookAndFeel = lookNFeel;
-			juce::Component::setLookAndFeel(mLookAndFeel.get());
-		}
+	/// @brief Draws this component to the screen
+	///
+	/// @param g - The graphics context to draw with
+	void ApexComboBox::paint(juce::Graphics& g) noexcept {
+		auto* temp = dynamic_cast<juce::Label*>(getChildComponent(0));
+		if(mLookAndFeel != nullptr) {
+			mLookAndFeel->drawApexComboBox(g,
+										   getWidth(),
+										   getHeight(),
+										   mIsButtonDown,
+										   temp->getRight(),
+										   0,
+										   getWidth() - temp->getRight(),
+										   getHeight(),
+										   *this);
 
-		Option<juce::Image> ApexComboBox::getActiveArrowImage() {
-			return mUsesArrowImages ? Option<juce::Image>::Some(mActiveArrowImage) :
-										Option<juce::Image>::None();
-		}
-
-		Option<juce::Image> ApexComboBox::getHoveredArrowImage() {
-			return mUsesArrowImages ? Option<juce::Image>::Some(mHoveredArrowImage) :
-										Option<juce::Image>::None();
-		}
-
-		Option<juce::Image> ApexComboBox::getNormalArrowImage() {
-			return mUsesArrowImages ? Option<juce::Image>::Some(mNormalArrowImage) :
-										Option<juce::Image>::None();
-		}
-
-		void ApexComboBox::paint(juce::Graphics& g) {
-			juce::Label* temp = static_cast<juce::Label*>(getChildComponent(0));
-			if(mLookAndFeel != nullptr) {
-				mLookAndFeel->drawApexComboBox(g,
-											   getWidth(),
-											   getHeight(),
-											   mIsButtonDown,
-											   temp->getRight(),
-											   0,
-											   getWidth() - temp->getRight(),
-											   getHeight(),
-											   *this);
-
-				if(getTextWhenNothingSelected().isNotEmpty() && temp->getText().isEmpty()
-				   && !temp->isBeingEdited())
-					mLookAndFeel->drawApexComboBoxTextWhenNothingSelected(g, *this, *temp);
+			if(getTextWhenNothingSelected().isNotEmpty() && temp->getText().isEmpty()
+			   && !temp->isBeingEdited()) {
+				mLookAndFeel->drawApexComboBoxTextWhenNothingSelected(g, *this, *temp);
 			}
 		}
-	} // namespace ui
-} // namespace apex
+	}
+} // namespace apex::ui
--- src/ui/ApexComboBox.h
+++ src/ui/ApexComboBox.h
@@ -5,58 +5,130 @@
 #include "../base/StandardIncludes.h"
 #include "ApexLookAndFeel.h"
 
-namespace apex {
-	namespace ui {
+namespace apex::ui {
+	class ApexComboBox : public juce::ComboBox {
+	  public:
+		/// @brief Constructs an `ApexComboBox` with the given drop down arrow images
+		///
+		/// @param activeArrowImage - The drop down arrow image to use when the drop down is active
+		/// @param hoveredArrowImage - The drop down arrow image to use when the combo box is
+		/// hovered
+		/// @param normalArrowImage - The drop down arrow image to use normally (ie, not active or
+		/// hovered)
+		ApexComboBox(juce::Image activeArrowImage,
+					 juce::Image hoveredArrowImage,
+					 juce::Image normalArrowImage) noexcept;
 
-		class ApexComboBox : public juce::ComboBox {
-		  public:
-			ApexComboBox(juce::Image activeArrowImage,
-						 juce::Image hoveredArrowImage,
-						 juce::Image normalArrowImage);
-			ApexComboBox();
+		/// @brief Constructs a default `ApexComboBox`
+		ApexComboBox() noexcept;
 
-			virtual void mouseDown(const juce::MouseEvent& e) override;
-			virtual void mouseUp(const juce::MouseEvent& e) override;
-			virtual void mouseEnter(const juce::MouseEvent& e) override;
-			virtual void mouseExit(const juce::MouseEvent& e) override;
+		/// @brief Mouse button pressed event handler
+		///
+		/// @param e - The mouse event to handle
+		auto mouseDown(const juce::MouseEvent& e) noexcept -> void override;
 
-			bool isHovered();
+		/// @brief Mouse button released event handler
+		///
+		/// @param e - The mouse event to handle
+		auto mouseUp(const juce::MouseEvent& e) noexcept -> void override;
 
-			void setHovered(bool hovered);
+		/// @brief Mouse entered component bounds handler
+		///
+		/// @param e - The mouse event to handle
+		auto mouseEnter(const juce::MouseEvent& e) noexcept -> void override;
 
-			void setXScaleFactor(float scaleFactor);
-			float getXScaleFactor();
-			void setYScaleFactor(float scaleFactor);
-			float getYScaleFactor();
+		/// @brief Mouse left component bounds handler
+		///
+		/// @param e - The mouse event to handle
+		auto mouseExit(const juce::MouseEvent& e) noexcept -> void override;
 
-			void setFont(juce::Font newFont);
+		/// @brief Returns whether the mouse is currently over this component
+		///
+		/// @return - Whether the mouse is over this
+		[[nodiscard]] inline auto isHovered() const noexcept -> bool;
 
-			juce::Font getFont();
+		/// @brief Force-sets whether the mouse is over this component
+		///
+		/// @param hovered - Whether the mouse is over this
+		inline auto setHovered(bool hovered) noexcept -> void;
 
-			void setLookAndFeel(std::shared_ptr<ApexLookAndFeel> lookNFeel);
+		/// @brief Sets the x-scale factor from the full size, useful when using graphics assets
+		/// instead of drawn elements to determine the ratio they should be scaled by
+		///
+		/// @param scaleFactor - The new scale factor
+		inline auto setXScaleFactor(float scaleFactor) noexcept -> void;
 
-			Option<juce::Image> getActiveArrowImage();
-			Option<juce::Image> getHoveredArrowImage();
-			Option<juce::Image> getNormalArrowImage();
+		/// @brief Returns the current x-scale factor from the full size, useful when using graphics
+		/// assets instead of drawn elements to determine the ratio they should be scaled by
+		///
+		/// @return - The current x-scale factor
+		[[nodiscard]] inline auto getXScaleFactor() const noexcept -> float;
 
-			virtual void paint(juce::Graphics& g) override;
+		/// @brief Sets the y-scale factor from the full size, useful when using graphics assets
+		/// instead of drawn elements to determine the ratio they should be scaled by
+		///
+		/// @param scaleFactor - The new scale factor
+		inline auto setYScaleFactor(float scaleFactor) noexcept -> void;
 
-		  protected:
-			std::shared_ptr<ApexLookAndFeel> mLookAndFeel;
-			juce::Image mActiveArrowImage;
-			juce::Image mHoveredArrowImage;
-			juce::Image mNormalArrowImage;
-			bool mUsesArrowImages = false;
+		/// @brief Returns the current y-scale factor from the full size, useful when using graphics
+		/// assets instead of drawn elements to determine the ratio they should be scaled by
+		///
+		/// @return - The current y-scale factor
+		[[nodiscard]] inline auto getYScaleFactor() const noexcept -> float;
 
-			bool mHovered = false;
-			float mXScaleFactor = 1.0f;
-			float mYScaleFactor = 1.0f;
-			juce::Font mFont;
+		/// @brief Sets the font size for text elements controlled/drawn by this component
+		///
+		/// @param newFont - The new font size
+		inline auto setFont(juce::Font newFont) noexcept -> void;
 
-			bool mIsButtonDown = false;
+		/// @brief Returns the current font size
+		///
+		/// @return - The current font size
+		[[nodiscard]] inline auto getFont() const noexcept -> juce::Font;
 
-		  private:
-			JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ApexComboBox)
-		};
-	} // namespace ui
-} // namespace apex
+		/// @brief Sets the look and feel of this component to the given one
+		///
+		/// @param lookNFeel - The new look and feel
+		inline auto setLookAndFeel(std::shared_ptr<ApexLookAndFeel> lookNFeel) noexcept -> void;
+
+		/// @brief Returns the drop down arrow image to use when the drop down is active, if this is
+		/// using graphics assets instead of drawn components
+		///
+		/// @return - The drop down arrow active image, or `None`
+		[[nodiscard]] inline auto getActiveArrowImage() const noexcept -> Option<juce::Image>;
+
+		/// @brief Returns the drop down arrow image to use when this is hovered, if this is
+		/// using graphics assets instead of drawn components
+		///
+		/// @return - The drop down arrow hovered image, or `None`
+		[[nodiscard]] inline auto getHoveredArrowImage() const noexcept -> Option<juce::Image>;
+
+		/// @brief Returns the drop down arrow image to use normally, if this is
+		/// using graphics assets instead of drawn components
+		///
+		/// @return - The drop down arrow normal image, or `None`
+		[[nodiscard]] inline auto getNormalArrowImage() const noexcept -> Option<juce::Image>;
+
+		/// @brief Draws this component to the screen
+		///
+		/// @param g - The graphics context to draw with
+		auto paint(juce::Graphics& g) noexcept -> void override;
+
+	  protected:
+		std::shared_ptr<ApexLookAndFeel> mLookAndFeel;
+		juce::Image mActiveArrowImage;
+		juce::Image mHoveredArrowImage;
+		juce::Image mNormalArrowImage;
+		bool mUsesArrowImages = false;
+
+		bool mHovered = false;
+		float mXScaleFactor = 1.0F;
+		float mYScaleFactor = 1.0F;
+		juce::Font mFont;
+
+		bool mIsButtonDown = false;
+
+	  private:
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ApexComboBox)
+	};
+} // namespace apex::ui
